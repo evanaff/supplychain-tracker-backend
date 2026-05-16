62ccc82baf50b08a1c6b66019740dc73ec153d86
EvanAhnafSatyatama <evan22002@mail.unpad.ac.id>
2026-05-16 21:30:55 +0700

    feat: add storage service

---
+++ src/services/storage/StorageService.ts
@@ -0,0 +1,33 @@
+import InvariantError from "../../common/exceptions/InvariantError";
+import { supabaseClient } from "../../lib/supabase";
+
+class StorageService {
+    async uploadImage(
+        fileBuffer: Buffer, 
+        originalName: string, 
+        folderName: string,
+        mimetype: string
+    ) {
+        const fileExtension = originalName.split('.').pop();
+        const filename = `${Math.random().toString()}.${fileExtension}`;
+        const filePath = `${folderName}/${filename}`;
+
+        const bucketName = "supplychain_tracker_bucket";
+
+        const { error } = await supabaseClient.storage.from(bucketName).upload(filePath, fileBuffer, {
+            cacheControl: '3600',
+            upsert: false,
+            contentType: mimetype
+        });
+
+        if (error) {
+            throw new InvariantError(`Failed to upload image: ${error.message}`);
+        }
+
+        const { data } = supabaseClient.storage.from(bucketName).getPublicUrl(filePath);
+
+        return data.publicUrl;
+    }
+}
+
+export default StorageService;

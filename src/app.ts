ae7c3eeb8fc9cadea1b48049e78fbfbba491f674
EvanAhnaf <evan.ahnaf.19@gmail.com>
2026-08-17 12:23:35 +0700

    chore: separate app and server for deployment

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import express, { type Request, type Response, type NextFunction } from 'express';
+import cors from 'cors';
+
+import config from './common/config/index.js';
+import ClientError from './common/exceptions/ClientError.js';
+
+import authRoutes from "./api/routes/auth.routes.js";
+import locationRoutes from "./api/routes/location.routes.js";
+import actorRoutes from "./api/routes/actor.routes.js";
+import productRoutes from "./api/routes/product.routes.js";
+import traceProductRoutes from "./api/routes/trace-product.routes.js"
+import traceEventRoutes from "./api/routes/trace-event.routes.js"
+import dashboardRoutes from "./api/routes/dashboard.routes.js"
+
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof ClientError) {
+        return res.status(err.statusCode).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+
+    console.error(err);
+    return res.status(500).json({
+        status: 'error',
+        message: 'An internal server error occurred'
+    });
+};
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Welcome to the Supply Chain Tracker API' });
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/locations', locationRoutes);
+app.use('/api/actors', actorRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/trace-products', traceProductRoutes);
+app.use('/api/trace-events', traceEventRoutes);
+app.use('/api/dashboard', dashboardRoutes);
+
+app.use(errorHandler);
+
+export default app;
